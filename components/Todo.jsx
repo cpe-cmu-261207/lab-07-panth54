b2f9451bb457c2c7d8cec25a31f793b1d51fda93
Phanthat Muenprap <phanthat54@gmail.com>
2022-08-18 22:06:39 +0700

    fin

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -7,5 +7,61 @@ import {
 } from "@tabler/icons";
 
 export default function Todo(props) {
-  return <div>your code here...</div>;
+  const [btnStyle, setBtnStyle] = useState({ display: "none" });
+
+  return (
+    <div>
+      <div
+        className="border-bottom p-1 py-2 fs-2 d-flex gap-2"
+        onMouseOver={() => {
+          setBtnStyle({ display: "" });
+        }}
+        onMouseOut={() => {
+          setBtnStyle({ display: "none" });
+        }}
+      >
+        <span
+          className="me-auto"
+          style={
+            props.completed
+              ? {
+                  textDecoration: "line-through",
+                }
+              : null
+          }
+        >
+          {props.title}
+        </span>
+
+        <button
+          className="btn btn-success"
+          style={btnStyle}
+          onClick={() => props.onMark()}
+        >
+          <IconCheck />
+        </button>
+        <button
+          className="btn btn-secondary"
+          style={btnStyle}
+          onClick={() => props.moveUp()}
+        >
+          <IconArrowUp />
+        </button>
+        <button
+          className="btn btn-secondary"
+          style={btnStyle}
+          onClick={() => props.moveDown()}
+        >
+          <IconArrowDown />
+        </button>
+        <button
+          className="btn btn-danger"
+          style={btnStyle}
+          onClick={() => props.onDelete()}
+        >
+          <IconTrash />
+        </button>
+      </div>
+    </div>
+  );
 }
